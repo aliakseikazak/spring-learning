f4556c946ae7b6c03364c4fd9dec50157c645349
Aliaksei Kazak <aliaksei_kazak@epam.com>
2019-09-24 10:19:56 +0300

    added example with init and destroy methods

--- src/main/java/by/kazak/spring/musicApp/beans/RapMusic.java
+++ src/main/java/by/kazak/spring/musicApp/beans/RapMusic.java
@@ -6,6 +6,14 @@ public class RapMusic implements Music {
     public RapMusic () {
     }
 
+    public void init () {
+        System.out.println(String.format("Initialisation of bean: %s", this.getClass().getSimpleName()));
+    }
+
+    public void destroy () {
+        System.out.println(String.format("Destruction of bean: %s", this.getClass().getSimpleName()));
+    }
+
     @Override
     public String getSong () {
         return String.format("%s: %s", this.getClass().getSimpleName(), SONG_NAME);
--- src/main/java/by/kazak/spring/musicApp/beans/RockMusic.java
+++ src/main/java/by/kazak/spring/musicApp/beans/RockMusic.java
@@ -6,6 +6,14 @@ public class RockMusic implements Music {
     public RockMusic () {
     }
 
+    public void init () {
+        System.out.println(String.format("Initialisation of bean: %s", this.getClass().getSimpleName()));
+    }
+
+    public void destroy () {
+        System.out.println(String.format("Destruction of bean: %s", this.getClass().getSimpleName()));
+    }
+
     @Override
     public String getSong () {
         return String.format("%s: %s", this.getClass().getSimpleName(), SONG_NAME);
